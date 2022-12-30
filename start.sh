40d7156a49fdc4016a472f1046cb5384d8a8a162
1426173978 <105538648+1426173978@users.noreply.github.com>
2022-12-30 13:38:47 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ PORT=10086
 UUID=cb22a209-a0b5-4bff-9535-80874122530d
 WebPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
 CaddyConfig=https://raw.githubusercontent.com/1426173978/northf/main/etc/Caddyfile
-XRayConfig=https://raw.githubusercontent.com/1426173978/northf/main/etc/xray.json
+XRayConfig=https://raw.githubusercontent.com/1426173978/northf/main/etc/xray.json?token=GHSAT0AAAAAAB42CVWGURKTBJCEI53HQX4IY5OPFIQ
 Xray_Newv=`wget --no-check-certificate -qO- https://api.github.com/repos/XTLS/Xray-core/tags | grep 'name' | cut -d\" -f4 | head -1 | cut -b 2-`
 # Install XRay
 mkdir -p /tmp/app
